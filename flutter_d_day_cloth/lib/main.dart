cade207963340d8d736237f5cec02bbe25781e75
this-is-yello <yello_3617@naver.com>
2022-12-08 19:07:18 +0900

    221208 in Company

--- flutter_d_day_cloth/lib/main.dart
+++ flutter_d_day_cloth/lib/main.dart
@@ -1,3 +1,6 @@
+import 'dart:math';
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -20,7 +23,10 @@ class MyApp extends StatelessWidget {
         ),
       ),
       floatingActionButton: ElevatedButton(
-        style: ElevatedButton.styleFrom(backgroundColor: Colors.black),
+        style: ElevatedButton.styleFrom(
+          backgroundColor: Colors.black,
+          minimumSize: Size(200, 80),
+        ),
         child: Text('대여하러가자', style: TextStyle(color: Colors.white)),
         onPressed: (() {
           Navigator.push(context,MaterialPageRoute(
@@ -58,27 +64,93 @@ class HomeScreen extends StatelessWidget {
       appBar: AppBar(
         automaticallyImplyLeading: false,
       ),
-      body:CustomScrollView(
-        slivers: [
-          SliverGrid(
-            delegate: SliverChildBuilderDelegate((c,i) =>
-              Container(color : Color(0xff3e3e3e)),
-              childCount: 12,
-            ),
-            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2, // 1개의 행에 항목을 3개씩
-              mainAxisSpacing: 10,
-              crossAxisSpacing: 10,
-              childAspectRatio: 1/1,
+      body: ListView(
+        children: [
+          Container(
+            padding: EdgeInsets.all(16),
+            child: Row(
+              children: [
+                Flexible(
+                  child: MouseRegion(
+                    cursor: SystemMouseCursors.click,
+                    child: GestureDetector(
+                      child: Container(
+                        height: 200,
+                        decoration: BoxDecoration(
+                          color: Colors.grey,
+                          borderRadius: BorderRadius.circular(15)
+                        ),
+                        child: Center(child: Text('면접대여'))
+                      ),
+                      onTap: () {
+                        Navigator.push(context, MaterialPageRoute(builder: (context) => MyApp()));
+                      }
+                    ),
+                  ),
+                  flex: 5
+                ),
+                Padding(padding: EdgeInsets.all(8)),
+                Flexible(
+                  child: MouseRegion(
+                    cursor: SystemMouseCursors.click,
+                    child: GestureDetector(
+                      child: Container(
+                        height: 200,
+                        decoration: BoxDecoration(
+                          color: Colors.grey,
+                          borderRadius: BorderRadius.circular(15)
+                        ),
+                        child: Center(child: Text('예복대여'))
+                      ),
+                      onTap: () {
+                        Navigator.push(context, MaterialPageRoute(builder: (context) => MyApp()));
+                      }
+                    ),
+                  ),
+                  flex: 5
+                ),
+              ],
             ),
           ),
-          SliverToBoxAdapter(
+          Flexible(
             child: Container(
-              width: 500, height: 500
-            )
+              margin: EdgeInsets.fromLTRB(16, 0, 16, 0),
+              height: 100,
+              decoration: BoxDecoration(
+                color: Colors.grey,
+                borderRadius: BorderRadius.circular(15)
+              ),
+            ),
+            flex: 10,
           ),
         ],
-      )
+      ),
     );
   }
 }
+
+
+// CustomScrollView(
+//   slivers: [
+//     SliverPadding(
+//       padding: EdgeInsets.symmetric(horizontal: 16),
+//     ),
+//     SliverGrid(
+//       delegate: SliverChildBuilderDelegate((c,i) =>
+//         Container(color : Color(0xff3e3e3e)),
+//         childCount: 2,
+//       ),
+//       gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+//         crossAxisCount: 2,
+//         mainAxisSpacing: 10,
+//         crossAxisSpacing: 10,
+//         childAspectRatio: 1/1,
+//       ),
+//     ),
+//     SliverToBoxAdapter(
+//       child: Container(
+//         width: 500, height: 500
+//       )
+//     ),
+//   ],
+// )
